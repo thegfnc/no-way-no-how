c8de4d90a7ba3e8f440dfb12f1c7c4c46bf3e79d
Jason Desiderio <jason@thegoodfornothings.club>
2025-05-06 15:07:02 -0500

    Update info on no way no how site

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import Image from 'next/image'
+import JasonGuitar from './jason-guitar.jpg'
+
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center gap-16 p-8'>
-      <div className='flex flex-col items-center gap-6'>
+    <main className='flex min-h-screen flex-col items-center justify-center gap-10 px-4 py-8 text-center md:gap-16 md:px-8'>
+      <div className='flex flex-col items-center gap-2 md:gap-6'>
         <h1 className='text-4xl font-semibold md:text-8xl'>no way no how</h1>
-        <div>music by Jason Desiderio</div>
+        <div>
+          <div>music by Jason Desiderio & friends in Austin, TX</div>
+        </div>
       </div>
-      <div className='w-full max-w-[480px]'>
+      <div className='grid w-full max-w-[900px] gap-6 md:grid-cols-2'>
         <iframe
           style={{ borderRadius: '12px' }}
           src='https://open.spotify.com/embed/artist/0Hhn7jlCTbqNyAOrI458SR?utm_source=generator'
@@ -16,22 +21,48 @@ export default function Home() {
           allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
           loading='lazy'
         />
-      </div>
-      <div>
-        <strong>Contact:</strong>{' '}
-        <a
-          href='mailto:hello@thegoodfornothings.club'
-          className='underline underline-offset-2 hover:no-underline'
-        >
-          hello@thegoodfornothings.club
-        </a>{' '}
-        &bull;{' '}
-        <a
-          href='https://soundcloud.com/no-way-no-how'
-          className='underline underline-offset-2 hover:no-underline'
-        >
-          Soundcloud
-        </a>
+        <Image
+          src={JasonGuitar}
+          alt='Jason Desiderio'
+          className='size-full rounded-xl object-cover'
+        />
+        <div>
+          <a
+            href='https://open.spotify.com/artist/0Hhn7jlCTbqNyAOrI458SR?si=3CrQhwPhToiFOQFg1ivtFQ'
+            className='underline underline-offset-2 hover:no-underline'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Spotify
+          </a>{' '}
+          &bull;{' '}
+          <a
+            href='http://music.apple.com/us/artist/no-way-no-how/1738953726'
+            className='underline underline-offset-2 hover:no-underline'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Apple Music
+          </a>{' '}
+          &bull;{' '}
+          <a
+            href='https://nowaynohow.bandcamp.com/'
+            className='underline underline-offset-2 hover:no-underline'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Bandcamp
+          </a>
+        </div>
+        <div>
+          <strong>Contact:</strong>{' '}
+          <a
+            href='mailto:hello@thegoodfornothings.club'
+            className='underline underline-offset-2 hover:no-underline'
+          >
+            hello@thegoodfornothings.club
+          </a>{' '}
+        </div>
       </div>
     </main>
   )
